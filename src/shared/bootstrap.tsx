057be9b55e3532e457cc26a17246ddcde5935150
afeiship <1290657123@qq.com>
2023-07-10 21:59:52 +0800

    wip

--- src/shared/bootstrap.tsx
+++ src/shared/bootstrap.tsx
@@ -2,7 +2,6 @@ import { $api } from '@/shared/services';
 import '@jswork/next-admin-kits';
 import * as AcComponents from '@jswork/antd-components';
 import { installWidgets } from '@jswork/antd-form-builder';
-import { KV_NAME_PAIRS } from '@/shared/constants';
 
 // AcComponents.AcSelect.defaultProps.kv = KV_NAME_PAIRS;
 // AcComponents.AcTree.defaultProps.kv = KV_NAME_PAIRS;
