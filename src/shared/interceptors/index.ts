c4430a22a349a543be2f67797bb79034b0e2c257
afeiship <1290657123@qq.com>
2023-07-10 21:34:53 +0800

    wip

--- src/shared/interceptors/index.ts
+++ src/shared/interceptors/index.ts
@@ -1,13 +1,11 @@
 import Auth from './request/auth';
 import BusError from './response/bus-error';
 import Business from './response/business';
-import Error from './error/common';
 import jzyunqiInterceptors from '@jswork/jzyunqi-interceptors';
 
 export const interceptors = [
   { type: 'request', fn: Auth },
   { type: 'response', fn: BusError },
   { type: 'response', fn: Business },
-  { type: 'error', fn: Error },
   ...jzyunqiInterceptors
 ];
