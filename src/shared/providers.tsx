bb91e35a9106778017042ae384bbfce804950e44
afeiship <1290657123@qq.com>
2023-07-12 22:26:47 +0800

    fix: rm unused query queryClient

--- src/shared/providers.tsx
+++ src/shared/providers.tsx
@@ -1,5 +1,5 @@
 import { HashRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ConfigProvider } from '@jswork/react-mobxer';
 import viteRequireContext from '@jswork/vite-require-context';
 
